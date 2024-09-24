1aca02b60efd991c223019edda7731872829358c
Aryankesharwani04 <2k22.cse.2213575@gmail.com>
2024-09-24 18:19:59 +0530

    updating list bullets

--- src/components/Achivement.tsx
+++ src/components/Achivement.tsx
@@ -3,8 +3,9 @@ function Achivement() {
   return (
     <div className="media pb-4">
       <h1 className="text-3xl font-bold mb-4 text-[var(--primary-300)]">Achievements</h1>
-      <ul className="text-[var(--text-200)]">
-        <li>Solved 400+ Problems on <a className="text-[var(--text-100)] font-bold" href="https://leetcode.com/aryankesharwani04/"><u>LeetCode</u></a>. <br />Achieved a badge for 200 days.</li>
+      <ul className="list-disc pl-6 text-[var(--text-200)]">
+        <li>Solved 400+ Problems on <a className="text-[var(--text-100)] font-bold" href="https://leetcode.com/aryankesharwani04/"><u>LeetCode</u></a>.</li>
+        <li>Achieved a badge for 200 days in <a className="text-[var(--text-100)] font-bold" href="https://leetcode.com/aryankesharwani04/"><u>LeetCode</u></a>.</li>
         <li>Achieved top 30% position in <a className="text-[var(--text-100)] font-bold" href="https://leetcode.com/aryankesharwani04/"><u>LeetCode</u></a> contests.</li>
         <li>4 stars in Problem Solving at <a className="text-[var(--text-100)] font-bold" href="https://www.hackerrank.com/profile/CS_2201640100088"><u>Hackerrank</u></a>.</li>
       </ul>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,7 +6,7 @@ function Project() {
       <div className="">
         <div className="box">
           <a href="https://github.com/Aryankesharwani04/Journee"><h2 className="text-xl font-bold text-[var(--accent-200)]"><u>Journee</u></h2></a>
-          <ul className="text-[var(--text-200)]">
+          <ul className="list-disc text-[var(--text-200)] pl-4 text-justify">
             <li> Developed a Tours and Travels Booking Website using the MERN stack.</li>
             <li>Designed and implemented the frontend using ReactJS.</li>
             <li>Created models for login, registration, and tour reviews using ExpressJS and MongoDB.</li>
@@ -18,7 +18,7 @@ function Project() {
         </div>
         <div className="box">
           <a href="https://github.com/Aryankesharwani04/javascriptBackend/tree/main/VideoStreaming"><h2 className="text-xl font-bold text-[var(--accent-200)]"><u>VideoFlix</u> <span>(In Progress)</span></h2></a>
-          <ul className="text-[var(--text-200)]">
+          <ul className="list-disc text-[var(--text-200)] pl-4 text-justify">
             <li>Currently developing a video streaming application to learn and implement backend technologies.</li>
             <li> Using Express.js and tools like Postman, Cloudinary, MongoDB Atlas to manage backend operations and database interactions.</li>
             <li>Enhancing back-end development skills by handling various server-side tasks and integrating APIs.</li>
@@ -26,7 +26,7 @@ function Project() {
         </div>
         <div className="box">
           <a href="https://github.com/Aryankesharwani04/WebScrapping/tree/main/attendanceGetter"><h1 className="text-xl font-bold text-[var(--accent-200)]"><u>Attendance Scraping</u></h1></a>
-          <ul className="text-[var(--text-200)]">
+          <ul className="list-disc text-[var(--text-200)] pl-4 text-justify">
             <li>Designed a web scraping tool using Node.js Puppeteer to automate the retrieval of attendance data from the college ERP portal.</li>
             <li>Deployed automated login to securely handle user authentication. </li>
             <li>Extracted and parsed HTML content to get total lecture and total absents.</li>
@@ -35,7 +35,7 @@ function Project() {
         </div>
         <div className="box">
           <a href="https://github.com/Aryankesharwani04/javascriptBackend/tree/main/CHESS"><h1 className="text-xl font-bold text-[var(--accent-200)]"><u>Chess Game</u></h1></a>
-          <ul className="text-[var(--text-200)]">
+          <ul className="list-disc text-[var(--text-200)] pl-4 text-justify">
             <li>Built a live chess game using Express.js and Socket.io for real-time communication between users.</li>
             <li>Engineered chess moves logic and managed game state using Chess.js CDN.</li>
             <li>Utilized EJS to render content dynamically based on backend data, enhancing the user experience.</li>
